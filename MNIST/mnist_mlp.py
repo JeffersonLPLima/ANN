5b44bd375594679430e29d94d49349f1d075a6ca
Jefferson Luiz Pessoa Lima <jeffersonlima163@gmail.com>
2017-11-23 01:40:42 -0200

    Update mnist_mlp.py

--- MNIST/mnist_mlp.py
+++ MNIST/mnist_mlp.py
@@ -13,18 +13,13 @@ num_classes = 10
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data() #download from https://s3.amazonaws.com/img-datasets/mnist.npz
 
-
-
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
@@ -37,15 +32,9 @@ model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(), metrics=['accuracy'])
 
-history = model.fit(x_train, y_train,
-                    batch_size = 128,
-                    epochs=20,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
+history = model.fit(x_train, y_train, batch_size = 128, epochs=20,verbose=1, validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
