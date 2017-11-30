8934d7d8773b0b0cac2a3817d6d8ac08a7480fad
Jefferson Luiz Pessoa Lima <jeffersonlima163@gmail.com>
2017-11-30 16:36:08 -0300

    Update mnist_mlp.py

--- MNIST/mnist_mlp.py
+++ MNIST/mnist_mlp.py
@@ -22,9 +22,21 @@ x_test = x_test.astype('float32') #transformando os píxels das imagens em float
 x_train /= 255    #fixando intervalo [0, 1]
 x_test /= 255   #fixando intervalo [0, 1]
 
+x_val = x_train[50000:60000]
+y_val = y_train[50000:60000]
+
+
+
+x_train = x_train[0:50000]
+y_train = y_train[0:50000]
+
+
+print(y_train[0:100])
 
 y_train = keras.utils.to_categorical(y_train, num_classes)  #convertendo as classes em vetores binários (one hot encoding)
 y_test = keras.utils.to_categorical(y_test, num_classes)  
+y_val = keras.utils.to_categorical(y_val, num_classes)  
+
 
 model = Sequential()
 model.add(Dense(512, activation='relu', input_shape=(784,)))
@@ -52,7 +64,7 @@ early_stopping=EarlyStopping(monitor='val_loss', #observar a função de perda n
 							verbose=1,)    
 																		
 																  #fazendo uso dos monitores criados		
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=[reduce_lr, early_stopping], verbose=1, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=[reduce_lr, early_stopping], verbose=1, validation_data=(x_val, y_val))
 
 loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
 
